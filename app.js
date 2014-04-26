09be1d13429f77920ab62823ea8b494559b60f7f
Zhang, Xiaolong <xiaolozhang@paypal.com>
2014-04-26 00:09:51 -0700

    Use monk

--- app.js
+++ app.js
@@ -9,20 +9,20 @@ var bodyParser = require('body-parser');
 // DataBase setup
 var mongo = require('mongodb');
 var monk = require('monk');
-//var db = monk('localhost:27017/nodetest1');
+var db = monk('ds031637.mongolab.com:31637/heroku_app24551371');
 
-var mongoose = require('mongoose');
-//var uristring = process.env.MONGOLAB_URI ||
-//    'localhost:27017/nodetest1';
-var uristring = 'ds031637.mongolab.com:31637/heroku_app24551371';
-
-mongoose.connect(uristring, function(err, res){
-    if(err){
-        console.log('ERROR connection to ' + uristring + '. ' + err);
-    } else{
-        console.log('Succeeded connected to: ' + uristring);
-    }
-});
+//var mongoose = require('mongoose');
+////var uristring = process.env.MONGOLAB_URI ||
+////    'localhost:27017/nodetest1';
+//var uristring = 'ds031637.mongolab.com:31637/heroku_app24551371';
+//
+//mongoose.connect(uristring, function(err, res){
+//    if(err){
+//        console.log('ERROR connection to ' + uristring + '. ' + err);
+//    } else{
+//        console.log('Succeeded connected to: ' + uristring);
+//    }
+//});
 
 
 // Routes setup
